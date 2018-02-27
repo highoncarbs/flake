3a26cd0f4b333e2b290fa9ce5983344d97192b17
padamsethia <padamsethia5@gmail.com>
2018-02-27 21:57:27 +0530

    Wesite updated

--- flake/flake.py
+++ flake/flake.py
@@ -30,11 +30,11 @@ def page(path):
     info.meta
     return render_template('page.html' , page = page ,username = info['name'] , desc = info['desc'], facebook = info['facebook'] , twitter = info['twitter'], github = info['github'], dribble = info['dribble'])
 
-@flake.route('/about')
+@flake.route('/about.html')
 def about():
     about = pages.get('about')
     about.meta
     info = pages.get('info')
     info.meta
-    return render_template('about.html',about = about , username = info['name'] ,
+    return render_template('about.html',page = about , username = info['name'] ,
      desc = info['desc'] , facebook = info['facebook'] , twitter = info['twitter'], github = info['github'], dribble = info['dribble'] ),200
